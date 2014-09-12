39e6f32129157d86a19be1a4ad9feef77dc9f8d9
Mario Villani <villani.mario90@gmail.com>
2014-09-12 13:09:01 +0200

    Removed some trackings
    Removed some Google Analytics calls, removed news and first install
    welcome screen.

--- fancy-settings/source/settings.js
+++ fancy-settings/source/settings.js
@@ -1,14 +1,3 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-42666021-1']);
-_gaq.push(['_gat._anonymizeIp']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 function set_icon(state) {
   if (state) {
     chrome.extension.sendRequest({action: 'quickEnable'})
--- popup.js
+++ popup.js
@@ -1,14 +1,3 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-35892158-1']);
-_gaq.push(['_gat._anonymizeIp']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})()
-
 document.addEventListener('DOMContentLoaded', function() {
     var settings = new Store('settings', {
       'show_mark_as_read': true,
@@ -20,20 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
     if (settings.get('block_chat_seen')) {
       chrome.browserAction.setIcon({path: 'icon48.disabled.png'})
       settings.set('block_chat_seen', false)
-      _gaq.push(['_trackEvent', 'Popup', 'quickToggle', 'disabled'])
       var text = document.createTextNode('Facebook unseen disabled. All your friends will now see if you read their messages.')
     } else {
       chrome.browserAction.setIcon({path: 'icon48.png'})
       settings.set('block_chat_seen', true)
-      _gaq.push(['_trackEvent', 'Popup', 'quickToggle', 'enabled'])
       var text = document.createTextNode('Facebook enabled. Your friends will not be notified when you read their messages.')
     }
     $('#message').html(text);
-
-    $.get('https://googledrive.com/host/0B16zq1ww34D9d3drWlJlUkdnX00/', function(data) {
-      $('#news').html(data);
-      $('#news a').click(function() {
-        _gaq.push(['_trackEvent', 'Popup', 'newsLinkClicked', $(this).text() + ' - ' + $(this).attr('href')]);
-      });
-    });
 })
